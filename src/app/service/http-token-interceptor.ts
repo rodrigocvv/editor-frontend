7a041f124e4b952ed90a2f305b34c288bb93fdda
Rodrigo Villela <rodrigocvv@gmail.com>
2018-11-12 07:22:53 -0200

    desenv

--- src/app/service/http-token-interceptor.ts
+++ src/app/service/http-token-interceptor.ts
@@ -11,12 +11,12 @@ export class HttpTokenInterceptor implements HttpInterceptor {
     constructor(private sessionService: SessionService) {}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        console.log('***************************************************');
-        console.log('***************************************************');
-        console.log('***************************************************');
-        console.log('***************************************************');
-        console.log('url => '+ req.url);
-        console.log('Meu token => ' + this.sessionService.token)
+        // console.log('***************************************************');
+        // console.log('***************************************************');
+        // console.log('***************************************************');
+        // console.log('***************************************************');
+        // console.log('url => '+ req.url);
+        // console.log('Meu token => ' + this.sessionService.token)
         // const accessToken = localStorage.getItem('access_token');
         const accessToken = this.sessionService.token;
         if (accessToken && req.url.includes('/data')) {
