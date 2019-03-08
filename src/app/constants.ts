de183085c498736d3dedabba3359b025d53d121e
Rodrigo Villela <rodrigocvv@gmail.com>
2019-03-07 22:35:51 -0300

    Audit Fix

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,7 +1,7 @@
-// export const constants = {
-//     url_backend: 'http://localhost:3000/'
-// };
-
+/* export const constants = {
+    url_backend: 'http://localhost:3000/'
+};
+ */
 export const constants = {
     url_backend: 'https://texteditor-backend.herokuapp.com/'
 };
